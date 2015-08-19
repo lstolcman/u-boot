0e222df6838900f602fe6bd504e4696da9976565
kozlopaw <pawel.kozlowski@tieto.com>
2015-08-19 07:49:17 +0200

    modified cmd_xyz.c

--- common/cmd_xyz.c
+++ common/cmd_xyz.c
@@ -1,40 +1,25 @@
 #include <common.h> 
-<<<<<<< HEAD
 #include <command.h> 
 #include<net.h>
-
 
 int do_connect(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[]) 
 { 
    char sendline[1024][3];
    int i;
-   net_init()
-
-
-   i = net_send_udp_packet(sendline, 10.0.0.1, 8080 , 9090, 1024);
-   sendline[0][0] = 0x01;
-   sendline[0][1] = 0x02;
-   semdline[0][1] = 0x01;
    
-
-
-   while (1)
+   for(;;)
    {
-
-      net_process_received_packet(uchar *in_packet, int len)
       switch((int)recvline)
         {
 	case 4:
 	sendline[0][0]= 0x02
 	sendline[0][1]= 0x00
-        i = net_send_udp_packet(sendline, 10.0.0.1, 8080 , 9090, 1024);
 	break;
 
         case 3:
 	sendline[0][0]= 0x01
 	sendline[0][1]= 0x01
         sendline[0][2]= 0x02
-        i = net_send_udp_packet(sendline, 10.0.0.1, 8080 , 9090, 1024);
 	goto change_status;
 	break;
 			
@@ -45,9 +30,10 @@ int do_connect(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
         default:
         printf("Incorrect message");
 	break;
-        } 	
-
+        }	
    }
+
+   change_status: 
 
 return 0;
 } 
