8a86d574bf64a686e236176e061cf3700359392f
Pawel Kozlowski <pawel.kozlowski@tieto.com>
2015-08-19 06:30:12 +0000

    cmd_xyz.c edited online with Bitbucket

--- common/cmd_xyz.c
+++ common/cmd_xyz.c
@@ -4,37 +4,6 @@
 
 int do_connect(cmd_tbl_t *cmdtp, int flag, int argc, char *argv[]) 
 { 
-   char sendline[1024][3];
-   char recvline[1024];
-   //int i;
-   
-   for(;;)
-   {
-      switch((int)recvline)
-        {
-	case 4:
-	sendline[0][0]= 0x02;
-	sendline[0][1]= 0x00;
-	break;
-
-        case 3:
-	sendline[0][0]= 0x01;
-	sendline[0][1]= 0x01;
-        sendline[0][2]= 0x02;
-	goto change_status;
-	break;
-			
-        case 2:
-	printf("Server response");
-        break;
-
-        default:
-        printf("Incorrect message");
-	break;
-        }	
-   }
-
-   change_status: 
 
 return 0;
 } 
